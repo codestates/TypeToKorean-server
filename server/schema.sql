a1d192f3fef7d33230c373ff2fa5619aeb78db6a
Sanghoon Han <bnnj86@gmail.com>
2019-10-30 14:56:47 +0900

    schema 수정.

--- server/schema.sql
+++ server/schema.sql
@@ -1,20 +1,23 @@
-CREATE TABLE `users` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(255),
-  `created_at` timestamp,
-  `password` varchar(255),
-  `email` varchar(255),
-  `phone` varchar(255),
-  `image` varchar(255)
+CREATE DATABASE typeToKorean;
+USE typeToKorean;
+
+CREATE TABLE users (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  username varchar(255),
+  created_at timestamp,
+  password varchar(255),
+  email varchar(255),
+  phone varchar(255),
+  image varchar(255)
 );
 
-CREATE TABLE `typeInformation` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `typeSpeed` int,
-  `score` int,
-  `typo` int,
-  `totaltime` int,
-  `userid` int
+CREATE TABLE typeInformation (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  typeSpeed int,
+  score int,
+  typo int,
+  totaltime int,
+  userid int
 );
 
-ALTER TABLE `typeInformation` ADD FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
+ALTER TABLE typeInformation ADD FOREIGN KEY (userid) REFERENCES users (id);
