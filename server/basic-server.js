2e805d326556ebf250e0f87df284a27e3d90462b
Sanghoon Han <bnnj86@gmail.com>
2019-10-30 17:31:40 +0900

    server test

--- server/basic-server.js
+++ server/basic-server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use('/', router);
 
 app.get('/', (req, res) => {
-  res.status(200).send('HelloWorld!, This is a TypeToKorean');
+  res.status(200).send('HelloWorld!, This is a TypeToKorean!');
 });
   
   
