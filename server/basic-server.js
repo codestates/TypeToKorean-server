a8f199455da16b7cc73b0a75bb5f71e1e278b7af
Sanghoon Han <bnnj86@gmail.com>
2019-10-30 17:16:28 +0900

    morgan, route, body-parser 설정.

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const http = require("http");
+
+//Middleware
 const cors = require("cors");
+const morgan = require("morgan");
+const parser = require("body-parser");
+
+//Router
+const router = require("./routes.js");
+
 const app = express();
 const port = 5000;
 
+app.set("port", port); // Set app, 5000 port;
 console.log('server is listen on',port);
 
+app.use(morgan('dev'));
+app.use(parser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {    
-    res.status(200).send("HelloWorld!, This is a TypeToKorean"); 
-  });
+app.use('/', router);
+
+app.get('/', (req, res) => {
+  res.status(200).send('HelloWorld!, This is a TypeToKorean');
+});
   
   
   app.get('/test', async (req, res) => {
     res.json({ message: 'pass!' });
   });
   
-  app.set("port", port);
+  
   app.listen(app.get("port"));
   
   module.exports = app;
