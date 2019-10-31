198c2c6932d5283b4e74d104ff943a1173d5b142
Sanghoon Han <bnnj86@gmail.com>
2019-11-01 03:02:58 +0900

    회원 전체 정보, 회원가입, 로그인(status400보완필요)

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,18 @@
 var router = require('express').Router();
 const usersTable = require("../models").users;
 const typeInformation = require("../models").urls;
+const crypto = require("crypto");
 
-// router.get('/users', controller.users.get);
-//회원전체 정보 
+
+router.get('/users', async (req, res) => {     //회원전체 정보 
+    let returnMessage = await usersTable.findAll();
+    res.status(200).send(returnMessage);
+});
+
 // router.get('/users/id', controller.userId.get);
 //회원 한명(id) 정보
-router.post('/users', async (req, res) => {
+
+router.post('/users', async (req, res) => {   //회원 가입
   // console.log("message post!!!")
   let body = req.body;
   console.log('req.body : ', body);
@@ -24,8 +30,48 @@ router.post('/users', async (req, res) => {
   });
   res.status(200).send(returnMessage[0]);
 });
-//회원 가입
-// router.post('/login', controller.login.post);
+
+
+router.post('/login', async (req, res) => {  // 회원 로그인
+
+  let body = req.body;
+  let sess = req.session;
+  console.log("body :",body);
+  console.log("sess", sess);
+  let hashPass = body.pw;
+  var shasum = crypto.createHash("sha1");
+  shasum.update(hashPass);
+  hashPass = shasum.digest("hex").slice(0, 5);
+  console.log("크립토암호화:",hashPass)
+
+  let findUserAndPassword = await usersTable.findAll({
+    where: {
+      email: body.email,
+      pw: hashPass
+    }
+  });
+  console.log("fuap",findUserAndPassword);
+  let findUser = await usersTable.findAll({
+    where: {
+      email: body.email
+    }
+  });
+
+  if (!findUser) {
+    // USERNAME NOT FOUND
+    res.status(200).send("USERNAME NOT FOUND");
+  }
+
+  if (findUserAndPassword[0].dataValues.pw === hashPass) {
+    sess.email = body.email;
+    let userId = { id: findUserAndPassword[0].dataValues.id };
+    res.status(200).send(userId);
+    console.log("sesschange", sess);
+  } else {
+    // req.id 랑 pw가 다른 경우
+    res.status(200).send("Wrong Access");
+  }
+});
 //회원 로그인
 // router.post('/logout', controller.logout.post);
 //회원 로그아웃
