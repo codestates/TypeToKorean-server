ef87a8f2c2201abca5f874e2c7b4d41d6e513e27
Hong Shik Branden Kim <kimploo@gmail.com>
2019-11-07 20:35:37 +0900

    1차 배포 전 로컬 호스트 변경

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,38 +1,40 @@
-const http = require("http");
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const parser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const router = require("./routes.js");
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const parser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const router = require('./routes.js');
 const app = express();
 const port = 5000;
-const session = require("express-session");
+const session = require('express-session');
 //db access => use sequelize
 // const db = require('./db');
 
-
-app.set("port", port); // Set app, 5000 port;
-console.log('server is listen on',port);
+app.set('port', port); // Set app, 5000 port;
+console.log('server is listen on', port);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin:
+      'http://typetokoreanclient.s3-website.ap-northeast-2.amazonaws.com/',
+    credentials: true,
+  }),
+);
 
 app.use(
   session({
-    key: "b6",
+    key: 'b6',
     secret: '@typeToKorean',
     resave: false,
     saveUninitialized: true,
     cookie: {
-      maxAge: 1000 * 60 * 60
-    }
+      maxAge: 1000 * 60 * 60,
+    },
   }),
 );
 
@@ -41,15 +43,11 @@ app.use('/', router);
 app.get('/', (req, res) => {
   res.status(200).send('HelloWorld!, This is a TypeToKorean!');
 });
-  
-  
-app.get('/test', async (req, res) => { 
+
+app.get('/test', async (req, res) => {
   res.json({ message: 'pass!' });
 });
-  
-  
+
 app.listen(app.get('port'));
 
 module.exports = app;
-
-
