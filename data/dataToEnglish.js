5a7fd70111222df12423835e528e7f6426b9bbf1
Sanghoon Han <bnnj86@gmail.com>
2019-11-08 22:36:26 +0900

    longSentence add

--- data/dataToEnglish.js
+++ data/dataToEnglish.js
@@ -19,8 +19,17 @@ let data = {
     ['There is no royal road to learning.'],
     ["Where there's a will, there's a way."],
     ['Heaven helps those who help themselves.'],
-    ['He is truly happy who makes otheres happy.'],
+    ['He is truly happy who makes otheres happy.']
   ],
+  longSentence: [
+    [
+      'My country’s language is different from Chinese, it doesn’t match their letters.',
+      'Therefore, even if my poor people want to do something,',
+      'many of them in the end can’t state their concerns.',
+      'Saddened by this state, I newly made 28 letters,',
+      'I wish that every man may easily learn, use daily, to make their lives to be convenient.',
+    ]
+  ]
 };
 
 module.exports = data;
