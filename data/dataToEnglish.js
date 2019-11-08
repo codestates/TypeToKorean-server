88ffa8e0c2c762c19512a9c4895f93e7e38f5fa2
Sanghoon Han <bnnj86@gmail.com>
2019-11-08 21:13:11 +0900

    sample route 추가

---
+++ data/dataToEnglish.js
@@ -0,0 +1,26 @@
+let data = {
+  shortSentence: [
+    ['Seeing is believing.'],
+    ['Whatever you do, make it pay.'],
+    ['You will never know until you try.'],
+    ['No sweat, no sweet.'],
+    ['Let bygones be bygones.'],
+    ['Do not count the eggs beore they hatch.'],
+    ['No pain no gain.'],
+    ['Life is venture or nothing'],
+    ['She can do, he can do, why not me?'],
+    ['Pain past is pleasure.'],
+    ['Time is gold.'],
+    ['Step by step goes a long way.'],
+    ['Rome was not built in a day.'],
+    ['Little by little does the trick.'],
+    ['To climb steep hills requires slow pace at first.'],
+    ['A rolling stone gathers no moss.'],
+    ['There is no royal road to learning.'],
+    ["Where there's a will, there's a way."],
+    ['Heaven helps those who help themselves.'],
+    ['He is truly happy who makes otheres happy.'],
+  ],
+};
+
+module.exports = data;
